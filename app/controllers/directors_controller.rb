1b41d279dbb4c127142e19134462e2facafc4a34
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-09-28 21:28:13 +0000

    adding directors show

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,5 +2,19 @@ class DirectorsController < ApplicationController
   
 def index 
     render({ :template => "/directors_templates/list"})
+  end
+  
+  def show 
+    
+    the_id = params.fetch("the_id")
+
+    the_record = Director.where({ :id => the_id})
+
+    @the_director = the_record.at(0)
+
+
+
+    render({ :template => "/directors_templates/details"})
+
   end 
 end 
