466249fc800e9640569292029aca0e7db20ab2dc
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-09-29 16:44:01 +0000

    added movie controller # list,details

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,13 +6,11 @@ def index
   
   def show 
     
-    the_id = params.fetch("the_id")
+    x_the_id = params.fetch("the_id")
 
-    the_record = Director.where({ :id => the_id})
+    the_record = Director.where({ :id => x_the_id})
 
     @the_director = the_record.at(0)
-
-
 
     render({ :template => "/directors_templates/details"})
 
